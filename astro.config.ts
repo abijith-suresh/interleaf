ae3cf5968ed0d753b68c556092b5eaa7f63c6d85
Abijith Suresh <58354654+abijith-suresh@users.noreply.github.com>
2026-09-09 19:38:46 +0530

    build: add sitemap (#207)

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,11 @@
+import sitemap from "@astrojs/sitemap";
 import solid from "@astrojs/solid-js";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solid()],
+  integrations: [solid(), sitemap()],
   site: "https://interleaf.page",
   vite: {
     plugins: [tailwindcss()],
